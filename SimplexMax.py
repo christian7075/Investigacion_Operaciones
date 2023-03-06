d36bc41c9623d1093aa180099eb66daa80869eb7
cristian <65404175+christian7075@users.noreply.github.com>
2023-03-05 23:23:34 -0500

    Update SimplexMax.py

--- SimplexMax.py
+++ SimplexMax.py
@@ -1,6 +1,6 @@
 from scipy.optimize import linprog
-obj = [-4, -6]  # Coefficients of the objective function to be minimized
-lhs_eq = [[2, 1]]  # Coefficients of the left-hand side of the equality constraint
-rhs_eq = [5]  # Right-hand side of the equality constraint
+obj = [-4, -6]  # Coeficioentes de la funcion objetivo a ser maximizado
+lhs_eq = [[2, 1]]  # Coeficientes de mano izquierda de la igualdad
+rhs_eq = [5]  # Coeficientes de mano derecha de la igualdad
 res = linprog(c=obj, A_eq=lhs_eq, b_eq=rhs_eq, bounds=(0, None))
 print(res)
